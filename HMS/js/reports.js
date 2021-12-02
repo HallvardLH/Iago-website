6c9d55c8de60d5b8b6843fc16a79be9c1dc69093
AsgeirAlbretsen <bjellanda@gmail.com>
2021-12-02 20:25:53 +0100

    Finished reports boilerplate

--- HMS/js/reports.js
+++ HMS/js/reports.js
@@ -1,15 +1,38 @@
 const reports = {
 
+    /**
+     * Requests a list of reports from the database, with a callback to reports.build_html.
+     */
     update_request: function() {
         networking.HTTPToDB({
             "query": {
-                "title": { $regex: "(?i)" + byId("searchBar").value + "(?-i)" }
+                "title": { $regex: "(?i)" + "" + "(?-i)" }
             }
-        }, "reports", chart.clean_data, "get", "albretsenconsulting");
+        }, "reports", reports.build_html, "get", "albretsenconsulting");
     },
 
-    build_html: function() {
+    /**
+     * 
+     * @param {A string, which is a list of report objects} reports 
+     * Goes through each report, then calls reports.build_report_row to generate each row of reports.
+     */
+    build_html: function(reports) {
+        reports = JSON.parse(reports);
+
+        reports.forEach(report => {
+            reports.build_report_row(report)
+        });
+    },
+
+    /**
+     * 
+     * @param {A report object} report 
+     * Takes a report, and adds it to the list of reports.
+     */
+    build_report_row: function(report) {
         
     }
 
 }
+
+reports.update_request();
